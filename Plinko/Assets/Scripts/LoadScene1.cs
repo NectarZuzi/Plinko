7a17d3242eab42dd4da69e17aedad440ce770a9f
Benjamin-Triplett <benjamin.triplett@onerail.io>
2021-04-04 09:15:41 -0400

    Added ball spawn on click

--- Plinko/Assets/Scripts/LoadScene1.cs
+++ Plinko/Assets/Scripts/LoadScene1.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LoadScene1 : MonoBehaviour
 {
     public GameObject peg;
+    public GameObject dropBall;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,17 @@ public class LoadScene1 : MonoBehaviour
                 int vertical = y * 2;
                 Instantiate(peg, new Vector3(horizontal, vertical, 0), Quaternion.identity);
             }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = Camera.main.nearClipPlane;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+            Instantiate(dropBall, worldPosition, Quaternion.identity);
         }
     }
 }
